c07001c15e02b9713973742fb158e4356f163932
hayasehiise <kuronegahiise@gmail.com>
2025-08-28 20:32:58 +0800

    sudah fix aman

--- app/Filament/Widgets/FilterLaporanPrint.php
+++ app/Filament/Widgets/FilterLaporanPrint.php
@@ -2,60 +2,23 @@
 
 namespace App\Filament\Widgets;
 
-use Carbon\Carbon;
-use Filament\Actions\Action;
-use Filament\Forms\Components\DatePicker;
-use Filament\Forms\Concerns\InteractsWithForms;
-use Filament\Forms\Contracts\HasForms;
-use Filament\Schemas\Schema;
 use Filament\Widgets\Widget;
 
-class FilterLaporanPrint extends Widget implements HasForms
+class FilterLaporanPrint extends Widget
 {
-    use InteractsWithForms;
 
     protected string $view = 'filament.widgets.filter-laporan-print';
 
     protected static bool $isLazy = false;
 
-    public ?array $data = [];
-
-    public function mount(): void
-    {
-        $this->form->fill([
-            'start_date' => Carbon::now()->startOfMonth(),
-            'end_date' => Carbon::now()->endOfMonth(),
-        ]);
-    }
-
-    public function form(Schema $schema): Schema
-    {
-        return $schema
-            ->schema([
-                DatePicker::make('start_date')
-                    ->label('Dari Tanggal'),
-                DatePicker::make('end_date')
-                    ->label('Sampai Tanggal'),
-                $this->printAction(),
-            ])
-            ->statePath('data');
-    }
-
-    public function printAction(): Action
-    {
-        return Action::make()
-            ->label('Print')
-            ->action('submit');
-    }
+    public ?string $start_date = null;
+    public ?string $end_date = null;
 
     public function submit()
     {
-        $start = $this->data['start_date'] ?? null;
-        $end = $this->data['end_date'] ?? null;
-
-        return redirect()->route('laporan.print', [
-            'start_date' => $start,
-            'end_date' => $end
+        return route('laporan.print', [
+            'start_date' => $this->start_date,
+            'end_date' => $this->end_date
         ]);
     }
 }
--- resources/views/filament/widgets/filter-laporan-print.blade.php
+++ resources/views/filament/widgets/filter-laporan-print.blade.php
@@ -2,6 +2,20 @@
     <x-filament::section>
         {{-- Widget content --}}
         <p class="text-lg font-bold mb-3">Print Laporan</p>
-        {{ $this->form }}
+        <form action="{{ $this->submit() }}" method="GET" target="_blank">
+            <div class="flex gap-5">
+                <x-filament::input.wrapper class="flex-1">
+                    <x-filament::input type="date" wire:model='start_date' name="start_date"
+                        value="{{ now()->startOfMonth() }}" />
+                </x-filament::input.wrapper>
+                <x-filament::input.wrapper class="flex-1">
+                    <x-filament::input type="date" wire:model='end_date' name="end_date"
+                        value="{{ now()->endOfMonth() }}" />
+                </x-filament::input.wrapper>
+            </div>
+            <x-filament::button type="submit" class="mt-4">
+                Print Laporan
+            </x-filament::button>
+        </form>
     </x-filament::section>
 </x-filament-widgets::widget>
